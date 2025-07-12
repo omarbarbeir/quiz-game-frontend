e62c36927f4090718cff1cbc81c5b96ace937b51
omarbarbeir <omarbarbeir2@gmail.com>
2025-07-13 01:02:45 +0300

    u

--- src/components/PlayerScreen.js
+++ src/components/PlayerScreen.js
@@ -135,7 +135,7 @@ const PlayerScreen = ({
 
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
 
-        {/* <div className="lg:col-span-1">
+        <div className="lg:col-span-1">
           <div className="bg-indigo-800 rounded-xl p-4 shadow-lg h-full">
             <h2 className="text-xl font-semibold mb-4">Leaderboard</h2>
             
@@ -166,7 +166,7 @@ const PlayerScreen = ({
               ))}
             </div>
           </div>
-        </div> */}
+        </div>
         
         <div className="lg:col-span-2 space-y-6">
           {gameStatus === 'ended' ? (
